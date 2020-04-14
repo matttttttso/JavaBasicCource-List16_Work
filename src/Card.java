30a615ae9550a5c56225f74d225c7f8e9dbd187a
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-14 15:04:21 +0900

    Println a Card tentatively
    ひとまずカードを１枚表示
    マークと数

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,18 @@
 
 public class Card {
+	//フィールドメンバ
+	String suit;	//4つのマーク（スペード、ハート、ダイヤ、クラブ）
+	int number;		//数字（1〜13）
 
+	//コンストラクタ
+	Card(String suit, int number){
+		this.suit = suit;
+		this.number = number;
+	}
+
+	//メソッドメンバ
+	//カードの表示
+	void showCard() {
+		System.out.println(this.suit + " の " + this.number);
+	}
 }
--- src/TrumpGame.java
+++ src/TrumpGame.java
@@ -1,4 +1,7 @@
 
 public class TrumpGame {
-
+	public static void main(String[] args) {
+		Card card = new Card("スペード",1);
+		card.showCard();
+	}
 }
