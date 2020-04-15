b4b76fd5719a04cf1614d077816e41abed406bf5
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-15 10:04:16 +0900

    Describe Chip Initializing

--- src/Chip.java
+++ src/Chip.java
@@ -1,4 +1,19 @@
 
 public class Chip {
+	//フィールドメンバ
+	int numChip10;	//[10]単位のチップの枚数
+	int numChip1;	//[1]単位のチップの枚数
+	int score;
 
+	//コンストラクタ
+	Chip(int num10, int num1){
+		this.numChip10 = num10;
+		this.numChip1 = num1;
+		score = (num10 * 10) + (num1 * 1);
+	}
+
+	//メソッドメンバ
+	String showChipNum() {
+		return "総計： "+ score + " ([10]:" + numChip10 + "枚, [1]:" + numChip1 + "枚)";
+	}
 }
--- src/TrumpGame.java
+++ src/TrumpGame.java
@@ -1,7 +1,19 @@
 
 public class TrumpGame {
 	public static void main(String[] args) {
+		Initialize();
+	}
+
+	static void Initialize() {
+		//ゲーム開始時の動作
+		//チップ初期化
+		Chip chip = new Chip(10, 0);
+		//カードを１枚配る
 		Card card = new Card("スペード",1);
-		card.showCard();
+		//表示
+		System.out.println("********チップ枚数とカード********");
+		System.out.println(chip.showChipNum());
+		System.out.println(card.showCard());
+		System.out.println("****************************");
 	}
 }
