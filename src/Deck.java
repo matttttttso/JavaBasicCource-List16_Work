54f7a8223965e8524e01d2b8344b51fedf765af5
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-15 14:51:04 +0900

    Describe Deck set,shuffle,showDeck

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,39 @@
+import java.util.Random;
 
 public class Deck {
+	int[] deck = new int[52];	//52個の行列作成
 
+	//行列に1~52の数字を順に入れていく
+	void setDeck() {
+		for(int i = 0; i < deck.length; i++) {
+			deck[i] = i + 1;
+		}
+	}
+
+	//シャッフルする
+	void shuffle() {
+		for(int i = 0; i < deck.length; i++) {
+			Random random = new Random();
+			int randNum = random.nextInt(52);
+			int tmp = deck[i];
+			deck[i] = deck[randNum];
+			deck[randNum] = tmp;
+		}
+	}
+
+	//デッキ内容表示（数字1~52で）
+	void showDeck() {
+		int count = 0;
+		for(int i = 0; i < deck.length; i++) {
+			System.out.print(deck[i] + "/");
+			count++;
+			if(count % 10 == 0) {
+				System.out.println("");
+			}
+		}
+	}
+
+	int deckToInt(int i) {
+		return deck[i];
+	}
 }
--- src/TrumpGame.java
+++ src/TrumpGame.java
@@ -1,19 +1,25 @@
 
 public class TrumpGame {
 	public static void main(String[] args) {
-		Initialize();
-	}
-
-	static void Initialize() {
-		//ゲーム開始時の動作
-		//チップ初期化
+		//ゲーム数（初期化）
+		int gameCount = 0;	//呼び出す配列の位置 ->0からスタート
+		//チップ数初期化
 		Chip chip = new Chip(10, 0);
-		//カードを１枚配る
-		Card card = new Card(2);
+		//デッキ初期化
+		Deck deck = new Deck();
+		deck.setDeck();	//デッキを整理
+		deck.shuffle();	//デッキのシャッフル
+//		deck.showDeck();	//デッキの内容表示
+
+		//デッキの数字をカード化
+		Card card = new Card(deck.deckToInt(gameCount));
+
 		//表示
 		System.out.println("********チップ枚数とカード********");
 		System.out.println(chip.showChipNum());
 		System.out.println(card.showCard());
 		System.out.println("****************************");
+
+		gameCount++;
 	}
 }
