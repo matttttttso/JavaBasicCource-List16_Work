72150a402bd6cc68182144393ad6cbb8e06da6cf
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-15 10:10:53 +0900

    Encupsulte and Describe Setter,Getter

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,21 @@
 
 public class Card {
 	//フィールドメンバ
-	String suit;	//4つのマーク（スペード、ハート、ダイヤ、クラブ）
-	int numCard;		//数字（1〜13）
+	private String suit;	//4つのマーク（スペード、ハート、ダイヤ、クラブ）
+	private int numCard;		//数字（1〜13）
+
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	public int getNumCard() {
+		return numCard;
+	}
+	public void setNumCard(int numCard) {
+		this.numCard = numCard;
+	}
 
 	//コンストラクタ
 	Card(String mark, int num){
--- src/Chip.java
+++ src/Chip.java
@@ -1,9 +1,28 @@
 
 public class Chip {
 	//フィールドメンバ
-	int numChip10;	//[10]単位のチップの枚数
-	int numChip1;	//[1]単位のチップの枚数
-	int score;
+	private int numChip10;	//[10]単位のチップの枚数
+	private int numChip1;	//[1]単位のチップの枚数
+	private int score;
+
+	public int getNumChip10() {
+		return numChip10;
+	}
+	public void setNumChip10(int numChip10) {
+		this.numChip10 = numChip10;
+	}
+	public int getNumChip1() {
+		return numChip1;
+	}
+	public void setNumChip1(int numChip1) {
+		this.numChip1 = numChip1;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
 
 	//コンストラクタ
 	Chip(int num10, int num1){
