c50d76d231fc806700f30a15831b0cbe82d61a1c
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-15 10:06:29 +0900

    Change naming variables

--- src/Card.java
+++ src/Card.java
@@ -2,17 +2,17 @@
 public class Card {
 	//フィールドメンバ
 	String suit;	//4つのマーク（スペード、ハート、ダイヤ、クラブ）
-	int number;		//数字（1〜13）
+	int numCard;		//数字（1〜13）
 
 	//コンストラクタ
-	Card(String suit, int number){
-		this.suit = suit;
-		this.number = number;
+	Card(String mark, int num){
+		this.suit = mark;
+		this.numCard = num;
 	}
 
 	//メソッドメンバ
 	//カードの表示
-	void showCard() {
-		System.out.println(this.suit + " の " + this.number);
+	String showCard() {
+		return "現在のカード： " + this.suit + " の " + this.numCard;
 	}
 }
