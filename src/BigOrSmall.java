7ec73a455b99979dcc72df8fb82d9f8e65623e52
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-17 11:24:35 +0900

    Refactoring BigOrSmall.java

--- src/BigOrSmall.java
+++ src/BigOrSmall.java
@@ -11,128 +11,88 @@ public class BigOrSmall {
 	int inputBetChip;
 	int getChip;
 	int choiceBigOrSmall;
-	int choiceContinueBet;
+	int choiceContinueBigOrSmall;
 	int choiceContinueGame;
 	String bigOrSmall;
-	String winOrLose = "Win!!";
+	String winOrLose;
 //フィールドメンバ	ここまで↑
 
 //プログラム記述		ここから↓
 	BigOrSmall(){
-		chip = new Chip(10, 0);					//チップ数初期化
-		while(choiceContinueGame == 0) {
-			while(winOrLose == "Win!!") {
-			//↓-----ゲーム数（初期化）------------------------------------------------------------↓
-				gameCount = 0;					//呼び出す配列の位置 ->0からスタート
-				deck = new Deck();				//デッキ初期化
-				deck.setDeck();					//デッキを整理
-				deck.shuffle();					//デッキのシャッフル
-				cardPrev = new Card(deck.deckToInt(gameCount));		//デッキからカードを1枚引く
-				cardNew = new Card(deck.deckToInt(gameCount + 1));	//次のカードも決めとく
-				initialPrint();					//初期チップ数、初期カード表示、BET数入力依頼
-			//↑-----ゲーム数（初期化）------------------------------------------------------------↑
-			//↓-----BET------------------------------------------------------------↓
-				checkInputBet();				//BET数入力、チェック
-				chip.decreaseChipNum(inputBetChip);					//BETした分減らす
-				System.out.println("BET数： " + inputBetChip);
-				System.out.println(chip.toString());
-			//↑-----BET------------------------------------------------------------↑
-			//↓-----BigOrSmall選択------------------------------------------------------------↓
-				printChoiceBigOrSmall();	//BigOrSmallの選択依頼
-				checkChoiceBigOrSmall();	//入力待機0:Big 1:Small
-			//↑-----BigOrSmall選択------------------------------------------------------------↑
-			//↓-----勝敗判定------------------------------------------------------------↓
-				judge();		//勝敗判定
-				printResult();	//結果表示
-			//↓-----勝敗判定------------------------------------------------------------↑
-			//↓-----負けたらループ脱出=>ゲームを続けるか選択-------------------------------------------↓
-				if(winOrLose == "Lose...") {
-					break;
-				}
-			//↑-----負けたらループ脱出=>ゲームを続けるか選択-------------------------------------------↑
-			//↓-----勝ったらループ続行------------------------------------------------------------↓
-				win();
-			//↓↓----BET続行確認-----------------------------------------------------------↓↓
-				checkChoiceContinueBet();
-			//↓↓----BET続行しない=>ループ脱出---------------------------------------------------------↓↓
-				if(choiceContinueBet == 1) {
-					System.out.println("BETを一旦やめます");
-					chip.increaseChipNum(getChip);
-					break;
-			//↑↑----BET続行しない=>ループ脱出---------------------------------------------------------↑↑
-			//↓↓----BET続行-----------------------------------------------------------↓↓
-				}else if(choiceContinueBet == 0) {
-					System.out.println("BET続けます");
-					inputBetChip = getChip;
-				}
-				while(choiceContinueBet == 0) {
-					gameCount++;	//ゲーム数を+1
-					cardPrev = new Card(deck.deckToInt(gameCount));
-					cardNew = new Card(deck.deckToInt(gameCount + 1));
-				//↓-----BigOrSmall選択------------------------------------------------------------↓
-					printChoiceBigOrSmall();
-					checkChoiceBigOrSmall();
-				//↑-----BigOrSmall選択------------------------------------------------------------↑
-				//↓-----勝敗判定------------------------------------------------------------↓
-					judge();
-					printResult();
-				//↓-----負けたらループ脱出=>ゲームを続けるか選択-------------------------------------------↓
-					if(winOrLose == "Lose...") {
-						break;
-					}
-				//↑-----負けたらループ脱出=>ゲームを続けるか選択-------------------------------------------↑
-					win();
-				//↓↓----BET続行確認-----------------------------------------------------------↓↓
-					checkChoiceContinueBet();
-				//↓↓----BET続行しない=>ゲームを続けるか選択---------------------------------------------------------↓↓
-					if(choiceContinueBet == 1) {
-						System.out.println("BETを一旦やめます");
-						chip.increaseChipNum(getChip);
-					//↓-----ゲームを続けるか選択-------------------------------------------↓
-						printContinueGame();
-						checkChoiceContinueGame();
-						if(choiceContinueGame == 1) {
-							System.out.println("ゲームをやめます");
+		chip = new Chip(10, 0);
+		while(true) {
+			while(true) {
+				initialize();
+				initialPrint();
+				checkInputBet();
+				chip.decreaseChipNum(inputBetChip);
+				printChoiceBigOrSmall();
+				checkChoiceBigOrSmall();
+				judge();
+				printResult();
+				if(winOrLose == "Win!!") {
+					while(true) {
+						win();
+						choiceContinueBIgOrSmall();
+						checkChoiceContinueBigOrSmall();
+						if(choiceContinueBigOrSmall == 0) {
+							System.out.println("連続BET!!");
+							inputBetChip = getChip;
+							gameCount++;
+							cardPrev = new Card(deck.deckToInt(gameCount));
+							cardNew = new Card(deck.deckToInt(gameCount + 1));
+							printChoiceBigOrSmall();
+							checkChoiceBigOrSmall();
+							judge();
+							printResult();
+							if(winOrLose == "Win!!") {
+								continue;
+							}else if(winOrLose == "Lose...") {
+								break;
+							}
+						}else if(choiceContinueBigOrSmall == 1) {
+							System.out.println("チップを回収します");
+							chip.increaseChipNum(getChip);
+							winOrLose = "Lose...";
 							break;
-						}else if(choiceContinueGame == 0){
-							System.out.println("ゲームを続けます");
 						}
-					//↑-----ゲームを続けるか選択-------------------------------------------↑
-				//↑↑----BET続行しない=>ループ脱出---------------------------------------------------------↑↑
-					}else if(choiceContinueBet == 0) {
-						System.out.println("BET続けます");
-						inputBetChip = getChip;
 					}
-				//↑↑----BET続行-----------------------------------------------------------↑↑
 				}
-				//↑-----勝ったらループ続行------------------------------------------------------------↑
-				//↓-----負けたらループ脱出=>ゲームを続けるか選択-------------------------------------------↓
 				if(winOrLose == "Lose...") {
 					break;
 				}
-				//↑-----負けたらループ脱出=>ゲームを続けるか選択------------------------------------------------------------↑
 			}
-			//↓-----ゲームを続けるか選択-------------------------------------------↓
+			if(chip.getScore() == 0) {
+				System.out.println("チップが底をつきました");
+				break;
+			}
 			printContinueGame();
 			checkChoiceContinueGame();
-			if(choiceContinueGame == 1) {
+			if(choiceContinueGame == 0){
+				System.out.println("ゲーム続行");
+				continue;
+			}else if(choiceContinueGame == 1) {
 				break;
-			}else if(choiceContinueGame == 0){
-				System.out.println("ゲームを続けます");
-				winOrLose = "Win!!";
 			}
-			//↑-----ゲームを続けるか選択-------------------------------------------↑
 		}
-		//プログラム終了
 		System.out.println("");
 		System.out.println("END");
 	}
 //プログラム記述		ここまで↑
 
 //メソッドメンバゾーン　ここから↓
+	void initialize() {
+		gameCount = 0;					//呼び出す配列の位置 ->0からスタート
+		deck = new Deck();				//デッキ初期化
+		deck.setDeck();					//デッキを整理
+		deck.shuffle();					//デッキのシャッフル
+		cardPrev = new Card(deck.deckToInt(gameCount));		//デッキからカードを1枚引く
+		cardNew = new Card(deck.deckToInt(gameCount + 1));	//次のカードも決めとく
+	}
 	//初期の表示
 	void initialPrint() {
-		System.out.println("********チップ枚数とカード********");
+		System.out.println("デッキを初期化しました");
+		System.out.println("\n********チップ枚数とカード********");
 		System.out.println(chip.toString());
 		System.out.println("現在のカード： " + cardPrev.toString());
 		System.out.println("****************************");
@@ -146,25 +106,30 @@ public class BigOrSmall {
 	void printChoiceBigOrSmall() {
 		System.out.println("\n" + "■Big or Small選択");
 		System.out.println("現在のカード： " + cardPrev.toString());
-		System.out.println("[Big or Small] 0:Big 1:Small");
+		System.out.println("[Big or Small]" + "\n0:Big 1:Small");
 	}
 	//結果表示
 	void printResult() {
-		System.out.println("********Big or Small********");
+		System.out.println("\n" + "<<<<<<<<Big or Small>>>>>>>>");
 		System.out.println(chip.toString());
 		System.out.println("BET数： " + inputBetChip);
 		choiceToString(choiceBigOrSmall);
 		System.out.println("引いたカード： " + cardNew.toString());
 		System.out.println(cardNew.toString() + " は " + cardPrev.toString() +" より " + bigOrSmall);
-		System.out.println("****************************");
-		System.out.println(winOrLose);
+		System.out.println("<<<<<<<<<<<<<<>>>>>>>>>>>>>>");
+		System.out.println("\n" + winOrLose);
 	}
 	//チップ数表示
 	void printContinueGame() {
-		System.out.println("********現在のチップ枚数********");
+		System.out.println("\n" + "--------現在のチップ枚数--------");
 		System.out.println(chip.toString());
-		System.out.println("****************************");
-		System.out.println("[ゲームを続けますか?] 0:Yes 1:No");
+		System.out.println("----------------------------");
+		if(chip.getScore() == 0) {
+			System.out.println("チップが底をつきました");
+		}
+		else if(chip.getScore() > 0) {
+			System.out.println("[ゲームを続けますか?]"+ "\n0:Yes 1:No");
+		}
 	}
 	//勝敗判定
 	void judge() {
@@ -195,6 +160,9 @@ public class BigOrSmall {
 	void win() {
 		getChip = inputBetChip * 2;
 		System.out.println("チップ" + getChip + "枚を獲得しました");
+	}
+	//連続でBig or Smallを続けるか確認
+	void choiceContinueBIgOrSmall() {
 		System.out.println("\n" + "[獲得したチップ" + getChip + "枚でBig or Smallを続けますか?] 0:Yes 1:No");
 	}
 	//Big or Small で選んだ方を文章化
@@ -245,11 +213,11 @@ public class BigOrSmall {
 			}
 		}
 	}
-	void checkChoiceContinueBet() {
+	void checkChoiceContinueBigOrSmall() {
 		while(true) {
 			try {
-				choiceContinueBet = new Scanner(System.in).nextInt();
-				checkChoice(choiceContinueBet);
+				choiceContinueBigOrSmall = new Scanner(System.in).nextInt();
+				checkChoice(choiceContinueBigOrSmall);
 				break;
 			} catch (NumberOutOfBoundException e) {
 				System.out.println(e.getMessage());
